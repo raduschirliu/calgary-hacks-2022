e766e3d0281767f0addb5e428c1d7a05be3a537f
Tyler Yip <tyler.yip1@ucalgary.ca>
2022-02-19 21:11:31 -0700

    feat: linking context and adding nav buttons

--- frontend/src/components/dashboard/WorkspaceContainer.tsx
+++ frontend/src/components/dashboard/WorkspaceContainer.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { WorkspaceContext } from '../../contexts/WorkspaceContext';
 import Leaderboard from './Leaderboard';
 import Profile from './Profile';
 import Taskboard from './Taskboard';
@@ -44,6 +45,8 @@ const sampleData = {
 };
 
 export default function WorkspaceContainer() {
+  const { currentWorkspace } = useContext(WorkspaceContext);
+
   return (
     <div className="container mx-auto px-4 col-span-3">
       <Profile user={sampleData.user} />
