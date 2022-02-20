1bfe7ccf9fca545bef8157f4458475f11d0658ad
Tyler Yip <tyler.yip1@ucalgary.ca>
2022-02-19 22:06:34 -0700

    feat: implemented more context functions

--- frontend/src/contexts/WorkspaceContext.tsx
+++ frontend/src/contexts/WorkspaceContext.tsx
@@ -2,6 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ITask from '../models/Task';
+import IUser from '../models/User';
 import { IWorkspace, IWorkspacePreview } from '../models/Workspace';
 
 interface IWorkspaceContext {
@@ -20,10 +21,12 @@ interface IWorkspaceContext {
 }
 
 const API_URL = process.env['REACT_APP_API_URL'] || 'localhost:8000';
-export const WorkspaceContext = React.createContext<IWorkspaceContext>(null as any);
+export const WorkspaceContext = React.createContext<IWorkspaceContext>(
+  null as any
+);
 
 export default function WorkspaceProvider({ children }: { children: any }) {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   /**
    * State
@@ -98,10 +101,96 @@ export default function WorkspaceProvider({ children }: { children: any }) {
   const inviteToWorkspace = (email: string) => {};
 
   // Add a new task to our current workspace (if valid)
-  const addTask = (task: ITask) => {};
+  const addTask = (task: ITask) => {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    axios
+      .post(
+        `${API_URL}/task`,
+        {
+          deadline: task.deadline,
+          difficulty: task.difficulty,
+          name: task.name,
+          category: task.category,
+          workspaces_id: currentWorkspace?.id,
+        },
+        { headers: baseHeader }
+      )
+      .then((res) => {
+        if (!currentWorkspace) {
+          return;
+        }
+        // Set the id of the newly created task
+        const addedTask: ITask = {
+          ...task,
+          id: res.data,
+        };
+
+        const updatedCurrentWorkspace: IWorkspace = {
+          ...currentWorkspace,
+          tasks: [...currentWorkspace.tasks, addedTask],
+        };
+        setCurrentWorkspace(updatedCurrentWorkspace);
+      })
+      .catch(console.error)
+      .finally(() => setIsLoading(false));
+  };
 
   // Update a task (if valid)
-  const updateTask = (task: ITask) => {};
+  const updateTask = (task: ITask) => {
+    if (isLoading || !isAuthenticated) return;
+    setIsLoading(true);
+
+    axios
+      .put(`${API_URL}/task/${task.id}`, {}, { headers: baseHeader })
+      .then((res) => {
+        // returns an updated score for the active user
+        if (!currentWorkspace || !user?.sub) {
+          return;
+        }
+
+        // Update the task as complete, and update the user's score
+        const updatedTask: ITask = {
+          ...task,
+          completedBy: [...task.completedBy, user.sub],
+        };
+
+        const currentUser = currentWorkspace.users.find(
+          (u) => u.id === user.sub
+        );
+        // This is a sanity check for typescript, theoretically there is no way the user isn't in the current workspace's list of users.
+        if (!currentUser) {
+          return;
+        }
+
+        const updatedUser: IUser = {
+          ...currentUser,
+          score: res.data,
+        };
+
+        const updatedCurrentWorkspace: IWorkspace = {
+          ...currentWorkspace,
+          tasks: currentWorkspace.tasks.map((t) => {
+            if (t.id === task.id) {
+              return updatedTask;
+            } else {
+              return t;
+            }
+          }),
+          users: currentWorkspace.users.map((u) => {
+            if (u.id === user.sub) {
+              return updatedUser;
+            } else {
+              return u;
+            }
+          }),
+        };
+        setCurrentWorkspace(updatedCurrentWorkspace);
+      })
+      .catch(console.error)
+      .finally(() => setIsLoading(false));
+  };
 
   /**
    * Effects
@@ -110,6 +199,7 @@ export default function WorkspaceProvider({ children }: { children: any }) {
   // Fetch workspaces on initial load
   useEffect(() => {
     updateWorkspaceList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Fetch JWT when it's updated
