6c98539fffab83da1636bf1867c949482cde9cc0
Radu Schirliu <radu.schirliu@gmail.com>
2022-02-20 10:30:11 -0700

    feat: add progress bars

--- frontend/src/components/dashboard/Leaderboard.tsx
+++ frontend/src/components/dashboard/Leaderboard.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import IUser from '../../models/User';
+import { AcademicCapIcon } from '@heroicons/react/solid';
 
 export default function Leaderboard({ users }: { users: IUser[] }) {
+  const sortedUsers = [...users].sort((a, b) => {
+    if (a.score > b.score) return -1;
+    if (a.score < b.score) return 1;
+    return 0;
+  });
+
+  const maxScore = sortedUsers.length > 0 ? sortedUsers[0].score : 0;
+
   return (
     <div className="mb-4 p-4 bg-white shadow-sm rounded-md">
       <p className="font-medium mb-2">Leaderboard</p>
 
       <div className="ml-2">
-        {users.map((user, index) => {
+        {sortedUsers.map((user, index) => {
+          const width = (user.score / maxScore) * 100;
+
           return (
             <div key={user.id} className="grid grid-cols-2">
-              <div>{user.name}</div>
-              <div>{user.score}</div>
+              <div className="flex flex-row">
+                {user.name}
+                {index === 0 ? <AcademicCapIcon className="ml-2 m-auto text-peach w-5 h-5" /> : <></>}
+              </div>
+
+              <div className="w-full h-4 bg-gray-200 rounded-full">
+                <div
+                  className="bg-peach text-xs font-medium text-white text-center p-0.5 leading-none rounded-l-full"
+                  style={{ width: `${width}%` }}
+                >
+                  {user.score} / {maxScore}
+                </div>
+              </div>
             </div>
           );
         })}
--- frontend/src/components/dashboard/TaskItem.tsx
+++ frontend/src/components/dashboard/TaskItem.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ITask from '../../models/Task';
 import IUser from '../../models/User';
+import { CheckIcon } from '@heroicons/react/solid';
+import { WorkspaceContext } from '../../contexts/WorkspaceContext';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function TaskItem({
   task,
@@ -9,22 +12,50 @@ export default function TaskItem({
   task: ITask;
   users: IUser[];
 }) {
-  function getCompletedByList() {
-    let names: string[] = [];
-    task.completedBy.map((completedUserId) => {
-      const userName = users.find((user) => user.id === completedUserId)?.name;
-      if (userName) {
-        names.push(userName);
+  const { user } = useAuth0();
+  const { updateTask } = useContext(WorkspaceContext);
+
+  let completedByMe = false;
+  let completed: IUser[] = [];
+
+  task.completedBy.map((completedUserId) => {
+    const cur = users.find((user) => user.id === completedUserId);
+    if (cur && !completed.includes(cur)) {
+      if (cur.id === user?.sub) {
+        completedByMe = true;
       }
-      return null;
-    });
-    return names.join(', ');
-  }
+
+      completed.push(cur);
+    }
+    return null;
+  });
+
+  const taskClicked = () => {
+    updateTask(task);
+  };
 
   return (
-    <div className="grid grid-cols-2">
-      <div>{task.name}</div>
-      <div>completed by: {getCompletedByList()}</div>
+    <div className="grid grid-cols-[1fr_1fr_120px]">
+      <span className="">{task.name}</span>
+      <div className="mr-4">
+        {users.length > 0 ? (
+          <span>completed by: {completed.map((u) => u.name).join(', ')}</span>
+        ) : (
+          <span></span>
+        )}
+      </div>
+
+      {completedByMe ? (
+        <></>
+      ) : (
+        <button
+          type="button"
+          className="inline-block px-4 py-1 border-2 border-peach text-peach font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+          onClick={taskClicked}
+        >
+          Mark Done
+        </button>
+      )}
     </div>
   );
 }
