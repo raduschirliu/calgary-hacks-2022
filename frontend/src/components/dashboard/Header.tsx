3dfdf9aba68241c29a723155028f7583928aefcc
Radu Schirliu <radu.schirliu@gmail.com>
2022-02-20 11:58:41 -0700

    fix: logout url

--- frontend/src/components/dashboard/Header.tsx
+++ frontend/src/components/dashboard/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
       >
         <MenuItem
           onClick={() => {
-            logout();
+            logout({ returnTo: window.location.origin });
             setAnchorEl(null);
           }}
         >
